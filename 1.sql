6aacf39b68e97cfa53232b2e4b02094b1d8d96ed
Kshitij Vijayvergiya <51355853+vijay-kshitij@users.noreply.github.com>
2024-01-27 19:23:52 -0500

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,8 +1,8 @@
 -- 1. What is the total amount each customer spent at the restaurant?
 
 SELECT
-  	sales.customer_id,
-    SUM(menu.price)
+  sales.customer_id,
+  SUM(menu.price)
 FROM dannys_diner.sales
 JOIN dannys_diner.menu ON sales.product_id = menu.product_id
 GROUP BY sales.customer_id
