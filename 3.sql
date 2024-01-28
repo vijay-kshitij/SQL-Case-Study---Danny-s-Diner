641afc656ae4b77fae212d21b3730dc3d1a8a572
Kshitij Vijayvergiya <51355853+vijay-kshitij@users.noreply.github.com>
2024-01-27 20:50:25 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -15,3 +15,17 @@ SELECT DISTINCT customer_id, product_name
 FROM temp_table
 WHERE row_num = 1
 
+
+-- Description of why can't we use just WHERE without using CTE.
+
+-- SELECT user_id, spend, transaction_date,
+-- ROW_NUMBER() OVER ( 
+--    PARTITION BY user_id
+--    ORDER BY transaction_date) AS row_num
+-- FROM transactions
+-- WHERE row_num = 3;
+
+-- ERROR: column "row_num" does not exist.
+-- The error is because the WHERE clause is trying to filter on the row_num alias, which is created in the SELECT statement 
+-- as part of the window function. In SQL, the WHERE clause is processed before the SELECT clause, so it doesn't "see" the aliases 
+-- defined in SELECT.
