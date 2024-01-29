d8e0e860829e9d0fed8a0442a3b236c0f88c6ce7
Kshitij Vijayvergiya <51355853+vijay-kshitij@users.noreply.github.com>
2024-01-28 19:09:06 -0500

    Add files via upload

---
+++ 6.sql
@@ -0,0 +1,18 @@
+-- 6. Which item was purchased first by the customer after they became a member?
+
+WITH temp_table AS (SELECT s.customer_id,
+s.order_date,
+f.product_name,
+RANK() OVER(PARTITION BY s.customer_id
+            ORDER BY s.order_date) AS rank_order
+FROM dannys_diner.sales AS s
+FULL JOIN dannys_diner.members AS m
+ON s.customer_id = m.customer_id
+JOIN dannys_diner.menu AS f
+ON s.product_id = f.product_id
+WHERE s.order_date >= m.join_date
+ORDER BY order_date
+)
+SELECT customer_id,product_name
+FROM temp_table
+WHERE rank_order=1;
